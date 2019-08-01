ba72039f7bc0809c1f198c6bcabe9e203d710369
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-01 20:52:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,11 @@ class TicTacToe
   def initialize
     @board = Array.new(9, " ")
   end
+
+  def new
+    self
+  end
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
